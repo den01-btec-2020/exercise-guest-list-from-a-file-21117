08272661939ca7aa8ed41ffc2dcde7068c66f6c3
jordonsm <21117@bridgend.ac.uk>
2020-11-06 10:49:31 +0000

    Guest list

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    
+    guests = ['Chuck Norris','Jack Baluer','Jack Bower']
+
+    name = guests[0].title()
+    print(name + ", The name is not on the list.")
+
+    name = guests[1].title() 
+    print(name + ", The name is not on the list.")
+
+    name = guests[2].title() 
+    print(name + ", The name is not on the list.")
+    
 
 if __name__ == '__main__':
     main()
