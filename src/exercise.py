37ac1d2af061606eba99d90fc0a51088c769cc3e
jordonsm <21117@bridgend.ac.uk>
2021-02-01 16:20:30 +0000

    I can't get it so that the user inputs a name.

--- src/exercise.py
+++ src/exercise.py
@@ -1,30 +1,37 @@
+import pytest
+import os
+
 def main():
     #write your code below this line
-    User_input = ("What is the name?")
+  
 
-    guests = ['ada','arto','leena','test','leo','jarmo','alicia']
+    input_value = ['ada','arto','leena','test','leo','jarmo','alicia']
+    
+    
+    
 
-    name = guests[0].title()
+    name = input_value[0].title()
     print(name + ", The name is not on the list.")
 
-    name = guests[1].title() 
+    name = input_value[1].title() 
     print(name + ", The name is not on the list.")
 
-    name = guests[2].title() 
+    name = input_value[2].title() 
     print(name + ", The name is not on the list.")
 
-    name = guests[3].title()
+    name = input_value[3].title()
     print(name + ", The name is not on the list.")
 
-    name = guests[4].title()
+    name = input_value[4].title()
     print(name + ", The name is not on the list.")
 
-    name = guests[5].title()
+    name = input_value[5].title()
     print(name + ", The name is not on the list.")
 
-    name = guests[6].title()
+    name = input_value[6].title()
     print(name + ", The name is not on the list.")
 
     print("Thank You")
+
 if __name__ == '__main__':
     main()
